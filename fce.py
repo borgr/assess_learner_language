29a3532c0e3792dce7bcbeea778fa59710fdc4eb
borgr <ktilana@gmail.com>
2016-08-21 12:23:49 +0300

    added dealing with fce corpus

---
+++ fce.py
@@ -0,0 +1,35 @@
+import re
+FCE_DATA_FILE = "en_esl-ud-all.conllu"
+CORRECTED_FILE = "corrected." + FCE_DATA_FILE
+LEARNER_FILE = "learner." + FCE_DATA_FILE
+
+def strip(line):
+	return line.replace("#SENT=","")
+
+def to_learner(xml_text):
+	sentence = re.sub("<c>.*?</c>", "", xml_text)
+	sentence = re.sub("<.*?>", "", sentence)
+	return sentence
+
+def to_corrected(xml_text):
+	sentence = re.sub("<i>.*?</i>", "", xml_text)
+	sentence = re.sub("<.*?>", "", sentence)
+	return sentence
+
+def main():
+	corrected = []
+	learner = []
+	with open(FCE_DATA_FILE) as fl:
+		for line in fl:
+			if line.startswith("#SENT="):
+				stripped = strip(line)
+				learner.append(to_learner(stripped))
+				corrected.append(to_corrected(stripped))
+
+	with open(CORRECTED_FILE,"w") as fl:
+		fl.writelines(corrected)
+	with open(LEARNER_FILE,"w") as fl:
+		fl.writelines(learner)
+
+if __name__ == '__main__':
+	main()
