2aa3faa702cb99c9657a9fe838279e42f34d19c5
borgr <ktilana@gmail.com>
2017-05-25 09:07:18 +0300

    more fix to the case where no upper scenes exists

--- distance_main.py
+++ distance_main.py
@@ -1,3 +1,6 @@
+import os
+import platform
+import traceback
 from multiprocessing import Pool
 import csv
 import sys
@@ -13,17 +16,32 @@ import align
 from ucca import layer0, layer1
 POOL_SIZE = 4
 PATH = r"/home/borgr/ucca/assess_learner_language/data/xmls/"
-trial_name = "parser"
+trial_name = "parser_r2s"
+UNCOMBINED_DIR = "uncombined/"
+corrected_stamp = "_corrected"
 
 filenames = []
 parsed_paragraphs = [2, 3, 5, 6, 7, 8, 10]
 passage_filenames = []
 for x in parsed_paragraphs:
 	passage_filenames.append(str(x))
-	passage_filenames.append(str(x) + "_corrected")
+	passage_filenames.append(str(x) + corrected_stamp)
 sys.setrecursionlimit(10000000)
 
-passage_filenames = [x + ".xml" for x in passage_filenames]
+# # combined passage names
+# passage_filenames = [x + ".xml" for x in passage_filenames]
+
+# sentence splitted xmls
+passage_filenames  = []
+for root, dirs, files in os.walk(PATH + UNCOMBINED_DIR):
+	for filename in files:
+		if filename.endswith(".xml"):
+			print(filename)
+			if corrected_stamp not in filename:
+				passage_filenames.append(UNCOMBINED_DIR + filename[:-7] + corrected_stamp + filename[-7:])
+				passage_filenames.append(UNCOMBINED_DIR + filename)
+print(passage_filenames)
+
 # borgr = list(("tree1197", "tree1297", "tree1198", "tree1298", "tree1200", "tree1300", "tree1202", "tree1302")) # "tree1299",  "tree1301"
 # amittaic = ["amittaic1197", "amittaic1297", "amittaic1200", "amittaic1300", "amittaic1198", "amittaic1298", "amittaic1205", "amittaic1305", "amittaic1203", "amittaic1303"] #, "amittaic1301"]
 # filenames = borgr + amittaic #same annotators
@@ -32,8 +50,8 @@ passage_filenames = [x + ".xml" for x in passage_filenames]
 # 			 "amittaic1300", "tree1300", "amittaic1198", "tree1198", "amittaic1298", "tree1298",
 # 			 "amittaic1301",  "tree1301"]#inter annotator
 # filenames = ["tree1297", "amittaic1297", "tree1298", "amittaic1298", "amittaic1300", "tree1300", "amittaic1301",  "tree1301"]
-implemented = [align.fully_aligned_distance]
-print("should flatten centers?")
+# implemented = [align.fully_aligned_distance]
+# print("should flatten centers?")
 
 #used functions
 funcs = [align.aligned_edit_distance, align.fully_aligned_distance, align.aligned_top_down_distance,
@@ -67,6 +85,7 @@ def main():
 	print (align.align("what has is by the meaning of the word is", "what is the men for the wk is are be"))
 
 	# read xml files
+	print("reading db xmls")
 	p = []
 	for filename in filenames:
 		with open(PATH + filename, "rb") as fl:
@@ -75,9 +94,18 @@ def main():
 	#convert xml to passages
 	p = list(map(convert.from_site,p))
 
+	print("reading passage xmls")
 	# read passage files
 	for filename in passage_filenames:
-		p.append(file2passage(PATH + filename))
+		print("reading" + filename)
+		if os.path.isfile(PATH + os.path.splitext(filename)[0] + ".pkl"):
+			with open(PATH + os.path.splitext(filename)[0] + ".pkl", "rb") as fl:
+				p.append(pickle.load(fl))
+		else:
+			p.append(file2passage(PATH + filename))
+			with open(PATH + os.path.splitext(filename)[0] + ".pkl", "wb") as fl:
+				pickle.dump(p[-1], fl)
+				print("dumping", PATH + os.path.splitext(filename)[0] + ".pkl")
 
 	all_filenames = filenames + passage_filenames
 	print("read ", all_filenames)
@@ -97,10 +125,15 @@ def main():
 		i += 1
 	print("multithreading")
 	pool = Pool(POOL_SIZE)
-	sym_mat, keys = pool.starmap(distances, zip(sources, goals, names))
+	results = pool.starmap(distances, zip(sources, goals, names))
+	print(results)
 	pool.close()
 	pool.join()
-
+	sym_mat = []
+	keys = []
+	for row, key in results:
+		keys.append(key)
+		sym_mat.append(row)
 	print("functions and matrix")
 	print(funcs+keys)
 	for item in sym_mat:
@@ -118,14 +151,16 @@ def distances(p1, p2, name):
 		res = [func(p1, p2) for func in funcs]
 		dic = complex_func(p1, p2)
 		keys = sorted(dic.keys())
-		res.insert(0, filename)
+		# print("funcs", funcs, file = sys.stderr)
+		# print("complex and keys", dic, keys, file = sys.stderr)
+		res.insert(0, name)
 		for key in keys:
 			res.append(dic[key])
 
 		print(res)
 		return res, keys
 	except Exception as e:
-		print("in",name)
+		print("in", name)
 		traceback.print_exc()
 		raise e
 
@@ -145,7 +180,20 @@ def distances(p1, p2, name):
 	# test(align.aligned_top_down_distance, p, sym=False)
 	# test(align.fully_aligned_distance, p) # heavy computations
 
-
+def anounce_finish():
+	if sys.platform == "linux":
+		if "Ubuntu" in platform.linux_distribution():
+			import subprocess
+			subprocess.call(['speech-dispatcher'])        #start speech dispatcher
+			subprocess.call(['spd-say', '"your process has finished"'])
+		else:
+			#perhaps works only in ubuntu?
+			os.system('play --no-show-progress --null --channels 1 synth %s sine %f' % ( 300, 2000))
+	if sys.platform == "darwin":
+		os.system('say "your program has finished"')
+	else:
+		import winsound
+		winsound.Beep(300,2000)
 if __name__ == '__main__':
 	main()
 
