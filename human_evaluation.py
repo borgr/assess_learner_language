9c01deab1363165bd94a0b515b224d8cd405d50f
borgr <ktilana@gmail.com>
2017-10-29 17:27:49 +0200

    evaluate UCCASim with human judgments

---
+++ human_evaluation.py
@@ -0,0 +1,7 @@
+# parse xmls note that id135 = line 136
+
+# rank outputs on each sentence that has human judgment using ucca and lt (and glue and m2 later)
+
+# think what to calculate with it (percentage of agreeing comparisons \ truekill \ expected wins both found in Human Evaluation of Grammatical Error Correction Systems)
+
+# rank using GLUE?
